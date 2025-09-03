6df43f2e07988d92e26f729e0537ed132b39b810
zepyroth3411 <brandones60@gmail.com>
2025-09-03 11:29:31 -0600

    feat: Se ve el tipo de variables y si forma de declaracion

---
+++ Variables/01-app.js
@@ -0,0 +1,15 @@
+// Inicializar una variable y asignarle un valor
+var producto = "Monitor 20 Pulgadas"
+console.log(producto);
+// Reasignar un valor a la misma variable
+producto = "Monitor 24 Pulgadas"
+console.log(producto);
+// JavaScript es un lenguaje de tipo dinámico, por lo que podemos reasignar diferentes tipos de datos
+producto = 20
+console.log(producto);
+
+//Forma tradicional de declarar una variable
+var nombreProducto = "Monitor 32 Pulgadas" // Camel Case, Es el uso mas comun en JavaScript
+var nombre_producto = "Monitor 32 Pulgadas" // Underscore o Snake Case, usado mas en Python
+var NombreProducto = "Monitor 32 Pulgadas" // Pascal Case Solo para clases 
+var nombreproducto = "Monitor 32 Pulgadas" // Lower Case 
