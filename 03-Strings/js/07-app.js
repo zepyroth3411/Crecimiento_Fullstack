434ed44a71d973ffd1dda23dfca9eca60431c9c5
zepyroth3411 <brandones60@gmail.com>
2025-09-04 15:18:06 -0600

    metodos touppercase y tolowercase

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,14 @@
+const producto = 'Monitor 20 pulgadas';
+
+
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+
+console.log(producto.toUpperCase().split("O"));// convierte el string a mayusculas y luego lo divide por la letra "O" y lo convierte en un array
+
+
+
+const precio1 =30
+const precio2 ="20"
+
+console.log(precio1+precio2); // 3020 (concatena los valores porque uno es string)
