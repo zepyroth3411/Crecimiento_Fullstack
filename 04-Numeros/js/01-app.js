51d73868790c7ecc8acb9c3e6e8829bf5fefad1a
zepyroth3411 <brandones60@gmail.com>
2025-09-04 15:26:40 -0600

    Numeros

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,13 @@
+const numero1= 20
+const numero2= "20"
+const numero3= 20.20
+
+console.log(numero1);
+console.log(numero2);   // String
+console.log(numero3);
+
+
+console.log(typeof numero1);
+console.log(typeof numero2);  // String 
+console.log(typeof numero3);
+
