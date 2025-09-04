5d2656fd8ec32ab3a48c92e305a104935d7f97bd
zepyroth3411 <brandones60@gmail.com>
2025-09-04 15:31:35 -0600

    Operaciones aritmeticas basicas

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,23 @@
+const numero1 = 20
+const numero2 = 30
+
+let resultado
+
+console.log("---- Operadores Aritméticos ----")
+
+// Suma
+resultado = numero1 + numero2
+console.log(`El resultado de la suma es: ${resultado}`)
+// Resta
+resultado = numero1 - numero2
+console.log(`El resultado de la resta es: ${resultado}`)
+// Multiplicación
+resultado = numero1 * numero2
+console.log(`El resultado de la multiplicación es: ${resultado}`)
+// División
+resultado = numero1 / numero2
+console.log(`El resultado de la división es: ${resultado}`)
+// Módulo
+resultado = numero1 & numero2
+console.log(`El resultado del módulo es: ${resultado}`)
+
