e19015ad5d48abc2b4375737fa1fc30e8977addf
zepyroth3411 <brandones60@gmail.com>
2025-09-10 16:14:12 -0600

    operador ternario

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,13 @@
+let edad = 20
+
+let puedeBeber 
+
+puedeBeber=   edad >= 18? true : false
+
+console.log(`¿Puede beber? ${puedeBeber}`)
+let hora = 22
+let saludar
+
+saludar = hora < 12 ? 'Buenos días' : hora < 18 ? 'Buenas tardes' : 'Buenas noches'
+
+console.log(saludar)
