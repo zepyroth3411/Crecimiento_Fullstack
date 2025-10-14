b7330a7935fc4743e5278c3fee2017e6fa99d681
zepyroth3411 <brandones60@gmail.com>
2025-10-14 14:25:35 -0600

    Eliminar elementos con splice

---
+++ 08-Arrays/js/07-app.js
@@ -0,0 +1,33 @@
+// se hablo de declaracion imperativa y declarativa
+// en la imperativa es una forma literal de crear un array
+const carrito = [
+    { nombre: "Monitor 32 Pulgadas", precio: 400 },
+    { nombre: "Celular", precio: 800 },
+    { nombre: "Teclado", precio: 50 },
+    { nombre: "Mouse", precio: 30 },
+];  
+
+//en la imperativa se utilizan los metodos de los arrays, modifican el array original
+carrito.push({ nombre: "Audifonos", precio: 200 });
+carrito.unshift({ nombre: "Motherboard", precio: 300 });
+console.table(carrito);
+
+//En la declaracion declarativa (spread operator)
+//la forma de interactuar con los array u objetos 
+const nuevoCarrito = [...carrito, { nombre: "Parlantes", precio: 400 }]; //agrega al final
+const nuevoCarrito2 = [{ nombre: "Escritorio", precio: 400 }, ...carrito]; //agrega al inicio
+
+console.table(nuevoCarrito);
+console.table(nuevoCarrito2);
+
+//eliminar ultimo elemento del array  
+carrito.pop();
+console.table(carrito); 
+
+//eliminar primer elemento del array
+carrito.shift();
+console.table(carrito);
+
+//eliminar un elemento en especifico (imperativa)
+carrito.splice(1, 1);   //elimina apartir del indice 1, 1 elemento
+console.table(carrito);
